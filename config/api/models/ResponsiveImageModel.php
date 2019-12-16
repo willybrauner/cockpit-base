0030ae8597c863fb6099423acc024c9dbe46e251
Willy <mail@willybrauner.com>
2019-12-16 22:10:35 +0100

    Manage default alt and caption

--- config/api/models/ResponsiveImageModel.php
+++ config/api/models/ResponsiveImageModel.php
@@ -22,44 +22,74 @@ class ResponsiveImageModel
     {
         // get meta asset id
         // allow to request the root asset field with additional information
-        $assetId = $pImage['meta']['asset'] ?? null;
+        $assetId = isset($pImage['meta']['asset']) ? $pImage['meta']['asset'] : null;
 
         // get asset field by id
         $rootAssetField = cockpit()->storage->findOne("cockpit/assets", [
             "_id" => $assetId
         ]);
 
-        // if meta title image in the gallery is set, set it
-        $alt = ($pImage['meta']['title'])
-            // else, show the root title of image (asset folder)
-            ?? $rootAssetField['title'];
+        // if meta title image in the gallery is set
+        if (isset($pImage['meta']['title']) && $pImage['meta']['title'] != "")
+        {
+            // use it
+            $alt = $pImage['meta']['title'];
+        }
+        // else if root asset field title is set
+        elseif (isset($rootAssetField['title']) && $rootAssetField['title'] != "")
+        {
+            // use it
+            $alt = $rootAssetField['title'];
+        }
+        else {
+            $alt = null;
+        }
 
-        // if meta desc image in the gallery is set, set it
-        $description = ($pImage['meta']['description'])
-            // else, show the root desc of image (asset folder)
-            ?? $rootAssetField['description'];
+        // if meta desc image in the gallery is set
+        if (isset ($pImage['meta']['description']) && $pImage['meta']['description'] != "")
+        {
+            // use it
+            $description = $pImage['meta']['description'];
+        }
+        // else if root desc exist
+        elseif (isset($rootAssetField['description']) && $rootAssetField['description'] != "")
+        {
+            // use it
+            $description = $rootAssetField['description'];
+        }
+        else {
+            $description = null;
+        }
 
         // get root asset path
-        $path = $rootAssetField['path'] ?? null;
+        $path = (isset($rootAssetField['path']) && $rootAssetField['path'] != "")
+            ? $rootAssetField['path']
+            : null;
+
         // get root asset colors
-        $colors = $rootAssetField['colors'] ?? null;
+        $colors = isset($rootAssetField['colors']) && count($rootAssetField['colors']) > 0
+            ? $rootAssetField['colors']
+            : null;
+
         // get root asset tags
-        $tags = $rootAssetField['tags'] ?? null;
+        $tags = isset($rootAssetField['tags']) && count($rootAssetField['tags']) > 0
+            ? $rootAssetField['tags']
+            : null;
 
         // creat final image object
         $imageObject = [
-           // "asset" => $rootAssetField,
+             //"asset" => $rootAssetField,
             "path" => $path,
             "colors" => $colors,
             "tags" => $tags,
             "alt" => $alt,
             "caption" => $description,
+            "_id" => $assetId,
             "data" => []
         ];
 
         // map each available size
-        foreach (ResponsiveImageModel::$sizes as $size)
-        {
+        foreach (ResponsiveImageModel::$sizes as $size) {
             // // check if image exist
             if (!isset($pImage["path"])) return;
 
